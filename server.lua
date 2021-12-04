e680e2c7f075786ad84c48740bee90dc0bc17afa
Pastor <71522613+Myrica-A@users.noreply.github.com>
2021-12-04 09:10:55 +0300

    version check added

--- server.lua
+++ server.lua
@@ -3,3 +3,32 @@ exports('Notify', function(args)
 	TriggerClientEvent('Pastor:SendNotify', args.target, args)
 end)
 
+-- version check
+Citizen.CreateThread( function()
+    if GetResourceMetadata(GetCurrentResourceName(), 'version') then
+        PerformHttpRequest(
+            'https://raw.githubusercontent.com/Pass-tor/Pastor_Notify/main/version.json',
+            function(code, res, headers)
+                if code == 200 then
+                    local rv = json.decode(res)
+                    if rv.version ~= GetResourceMetadata(GetCurrentResourceName(), 'version') then
+                        print(
+                            ([[^1-------------------------------------------------------
+                            Pastor_Notify
+UPDATE: %s AVAILABLE
+CHANGELOG: %s
+-------------------------------------------------------^0]]):format(
+                                rv.version,
+                                rv.changelog
+                            )
+                        )
+                    end
+                else
+                    print('Pastor_Notify unable to check version')
+                end
+            end,
+            'GET'
+        )
+    end
+end
+)
